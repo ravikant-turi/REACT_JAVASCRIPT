363a7e44961a9ccbb20dc5b7beb614d273bd550f
ravikant-turi <turiravikant@gmail.com>
2024-04-09 23:03:06 +0530

    start react

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,8 @@
+
+function Chai(){
+    return (
+        
+        <p>chia is ready par mitha hai</p>
+        
+    );
+}export default Chai()
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Chai() {
+  return (
+    <div>
+      <h1>chai is ready</h1>
+    </div>
+  )
+}
+
+export default Chai
